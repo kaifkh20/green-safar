2ce81dc2a444bfdfdab59bfa6b403a8bd74bf047
Farhan3439A <112370960+Farhan3439A@users.noreply.github.com>
2024-05-25 18:27:06 +0530

    inital commit

---
+++ schema/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password : {
+        type : String,
+        required : true
+    }
+})
+
+const User = mongoose.model('user',UserSchema)
+export default User;
